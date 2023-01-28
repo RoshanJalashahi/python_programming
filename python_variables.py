992957a3bbf90afdca0c5a78d475e06e1a5ef15c
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-28 23:02:48 +0545

    python variable declarations

---
+++ python_variables.py
@@ -0,0 +1,36 @@
+# variable are the containers that contains a data values
+# to create a varaible you just need to assign valiues to it
+''' for example '''
+x = 2
+y = 'Roshan'
+print(x)
+print(y)
+
+
+''' we can easily change the data  types of variables  for example '''
+x = 4
+x = 'prabina'
+print(x)
+
+''' varibles name are case sensitives i. e a and A are two different variables'''
+a = 25
+A = ' Prabina'
+print(a)
+print(A)
+
+
+''' cjhanging variables data types i.e type casting'''
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+''' we can also get the types of variables
+using type() function'''
+x = 5
+y = 'Prabina'
+print(type(x))
+print(type(y))
