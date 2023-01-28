71bd2bda71b518df807d299f6489ab5643858838
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-28 22:50:19 +0545

    how to comments on python

---
+++ python_comments.py
@@ -0,0 +1,7 @@
+# comments in python starts with # at the beginnig
+print('Hello,World ') # this is an example for a python program.
+#for the multiline comments pyhton usually don't have any syntax but you can write all the comments inside the 3 quotes
+''' for exampole  this is 
+a multiline comments . python actually reads all the strings but 
+since this string is not assign to any variable it will at the end ignore it like this '''
+print('comments , syntax')
