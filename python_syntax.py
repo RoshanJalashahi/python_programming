414e5b4c0d387110150fb1a60d8aafee27a8dc6b
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-28 22:40:50 +0545

    about indentation

---
+++ python_syntax.py
@@ -0,0 +1,17 @@
+# indentation is very important in python because python uses a indentation to indicate a block of code
+if 5 > 2:
+ print("5 is larger")
+
+ # for the same block of code the space i.e indentation must be same
+ #example
+ #if 5 > 2:
+     #print("5 is larger")
+        #print("5 is larger again")
+ # Here in the above example the error will occur
+ #CORRECTLY SYNTAX WILL BE
+ if 5 > 2:
+  print("5 is larger")
+  print("5 is larger")
+
+
+
