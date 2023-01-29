11c66d5931c30e90f50a7ea864eafc611f37e606
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-29 22:42:26 +0545

    python Castings

---
+++ python_castings.py
@@ -0,0 +1,25 @@
+''' cating is done in Python by using constructor'''
+# for integer
+# construct a integer number from a integer literals , from string literals and from float literals
+x = int(1)
+y = int(2.36)
+z =  int("36")
+print(x)
+print(y)
+print(z)
+
+ # construct afloat literals from a float literals , from int literals and from string literals
+x = float(2.6)
+y = float(2)
+z = float("36")
+print(x)
+print(y)
+print(z)
+
+# construct a string literals from a float and int literls as below
+x = str(2)
+y = str(2.36)
+z = str("336")
+print(x)
+print(y)
+print(z)
