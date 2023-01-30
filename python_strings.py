b2bdb4f1fbdc3849cd85e50949338a203e3af539
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-30 22:04:44 +0545

    python_string of love

---
+++ python_strings.py
@@ -0,0 +1,33 @@
+''' strings in python can be represented inside a single quatation or double quotation'''
+print("hello Parbina")
+print('hello,Prabina')
+''' multiline strings can be used by writing inside a three quotation or three dounble quotation'''
+a = ''' hi , i am a Roshan kumar chaudhary , i am python programmerr'''
+print(a)
+
+# string as a array
+x = 'Hello, World'
+print(x[1])
+
+# string as a loop
+for a in "Prabina":
+    print(a)
+
+# string length
+a = " Roshan"
+print(len(a))
+
+# check string
+why =' Hi i am Roshan .K chaudhary.. I am a programmer . Date 10-09 first walk with Prabina .'
+print("Prabina" in why)
+print("love" in why)
+
+# check string with the help of if
+why = ''' date 10-10 last walk with the prabina .. hope to never bother her again..'''
+if "prabina" in why:
+    print(" yes "+" it was our last walk")
+
+# check string not in
+why = ''' Hi Prabina , we are not mean to be made for each other ,, hope you the best life ahead in your future..'''
+print("Roshan" not in why )
+
