5b326ea05b8e5b59e9887ed4bebf35b352b4d369
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-30 22:14:25 +0545

    python_slicing_love

---
+++ python_slicing_string.py
@@ -0,0 +1,15 @@
+# string Sclicing
+a ="Hello,World!"  # here only the letters from position 2 to position 6(not included) will be shown
+print(a[2:6])
+
+# slice from the end
+a = "  Prabina, Roshan"
+print(a[2:])
+
+# sllice from the begin
+a = "Prabina , Roshan"
+print(a[:10])
+
+#negative indexing
+a = "i love Prabina "
+print(a[-8:-1])
