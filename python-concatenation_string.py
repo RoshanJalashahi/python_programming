f66e9689b0764c89459312c7291b633c3735ea23
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-30 22:27:28 +0545

    string_concatenation

---
+++ python-concatenation_string.py
@@ -0,0 +1,11 @@
+#string can be concatenated using + operator
+a = "Prabina"
+b = "Roshan"
+c = a+b
+print(c)
+
+# to add space between them we used ""
+a = "Prabina"
+b = "Roshan"
+c = a+" "+b
+print(c)
