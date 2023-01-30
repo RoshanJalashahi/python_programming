b41de30489022fdecfcfe83a2f5e23057160dbc9
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-30 22:23:47 +0545

    python_string_modify

---
+++ python_string_modification.py
@@ -0,0 +1,21 @@
+# string can be modify
+# the upper() method returns the string with uppercase
+a ="prabina"
+print(a.upper())
+
+# lower () method
+a ="HELLO, Prabina"
+print(a.lower())
+
+# removing whitespace using strip() method
+# this method removes spave from the beginning or from the end but not from the middle
+a =" Prabina, Roshan! "
+print(a.strip())
+
+#replace() method
+a = " hello, Hrabina"
+print(a.replace("H", "P"))
+
+#split() method
+a = " Roshan, Prabina"
+print(a.split(","))
