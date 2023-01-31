9efaf3a0289cfb48fa50d15a21af0328fad71771
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-31 22:11:50 +0545

    python_operators

---
+++ python_operator.py
@@ -0,0 +1,14 @@
+# there are several operators on the Python
+"""
+1) Arithmetic operator
+2) Assignment operator
+3) Comparison Operator
+4) Logical Operator
+5) identity Operator
+6) Membership Operator
+7) Bitwise Operator
+"""
+# Arithmetic Operator
+ # +,-,* ,/,**,//,%
+
+#Assignment Operator
