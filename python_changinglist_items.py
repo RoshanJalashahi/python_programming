d18cbc5f449f5c795ea95bb421f6179741e462eb
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-31 22:36:50 +0545

    changing_list

---
+++ python_changinglist_items.py
@@ -0,0 +1,9 @@
+#changing items in list
+mylist = ["Prabina","Hates","Roshan"]
+mylist[1] = "loves"
+mylist[2:3] = "forever","only me"
+print(mylist)
+
+mylist = ["Prabina","loves","Roshan"]
+mylist.insert(2,"Forever")
+print(mylist)
