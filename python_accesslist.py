0b1cb6997b14d84bfa3406ee5ddd6464f011d74a
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-01-31 22:30:26 +0545

    python_accessinglist

---
+++ python_accesslist.py
@@ -0,0 +1,11 @@
+#python accessing list
+thislist = ["Prabina","Roshan","Lovers","Forever","unseparable"]
+print(thislist[2])
+print(thislist[-5:-1])
+print(thislist[2:])
+ #check present in string
+thislist = ["Prabina","Roshan","Lovers","Forever","unseparable"]
+if "Lovers" in thislist:
+    print("yes, cant leave without eachother")
+else:
+    print("no it cant be false")
