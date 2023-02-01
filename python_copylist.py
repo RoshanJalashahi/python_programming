beb07fac48085cafc7c32fa3b51076fe3f03890c
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-01 22:09:45 +0545

    list_copying

---
+++ python_copylist.py
@@ -0,0 +1,8 @@
+#copy() used to copy list
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+mylist = list(thislist)
+print(mylist)
