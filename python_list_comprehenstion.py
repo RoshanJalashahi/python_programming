f7cf41decc52d60dff0e7c73cda32641b1fdb12d
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-01 21:56:57 +0545

    list_comprehenstion

---
+++ python_list_comprehenstion.py
@@ -0,0 +1,34 @@
+# comprehenstion
+fruits = ["appple","banana","cherry","Mango","Grapes"]
+newlist = []
+for x in fruits:
+    if "a" in x:
+        newlist.append(x)
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newl= [x for x in fruits if "a" in x]
+print(newl)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [ x for x in range(10) if x<5]
+print(newlist)
+newl = [x.upper() for x in fruits]
+print(newl)
+nwl = [ "hello" for x in fruits]
+print(nwl)
+nowl = [ x if x !="banana" else "orange" for x in fruits]
+print(nowl)
