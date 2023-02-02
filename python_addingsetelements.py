6774650c2cb72e585de604a19774b0c19f3cabfe
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 22:33:29 +0545

    set_addingelements

---
+++ python_addingsetelements.py
@@ -0,0 +1,13 @@
+set1 = {"prabina","loves","Roshan","forever"}
+set1.add("lovebiords")
+print(set1)
+
+set1 = {"prabina","loves","Roshan","forever"}
+set2 = {"they","are ","lovebirds"}
+set2.update(set1)
+print(set2)
+
+set1 = {"prabina","loves","Roshan","forever"}
+set2 = ["they","are","lovers"]
+set1.update(set2)
+print(set1)
