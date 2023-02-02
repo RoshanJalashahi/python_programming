cf15d3b5faf39bf5301c2bb41b19cd4ee57e4c2d
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 22:27:14 +0545

    python_setaccessing

---
+++ python_accessingsets.py
@@ -0,0 +1,10 @@
+'''You cannot access items in a set by referring to an index or a key.
+But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+'''
+thisset = {"prabina","loves","Roshan","forever"}
+for x in thisset:
+    print(x)
+
+
+thisset = {"prabina","loves","Roshan","forever"}
+print("loves" in thisset)
