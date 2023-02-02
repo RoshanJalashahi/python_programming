26719f5cf0c7cd7b092134a5f3c291cb488c8a0e
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 22:23:13 +0545

    python_sets

---
+++ python_sets.py
@@ -0,0 +1,17 @@
+thisset = {"Prabina","Loves","Roshan"}
+print(thisset)
+
+thisset = {"Prabina","Loves","Roshan","Forever","Loves"}
+print(thisset)
+print(len(thisset))
+
+set1 = {"prabina","loves","apple"}
+set2 = {1,3,4,9}
+set3 = {True,False,True}
+print(set1)
+print(set2)
+print(set3)
+
+#constructor
+fruits = set(("apple", "mango", "papaya", "pineapple", "cherry"))
+print(fruits)
