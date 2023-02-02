980ef806f71bb0bd1273792c938285d399175ddf
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 21:50:19 +0545

    tuples_access

---
+++ python_tuplesaccess.py
@@ -0,0 +1,8 @@
+tuple = ("Prabina","Roshan","Lovers")
+print(tuple[0])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+print(thistuple[-4:-1])
+if "apple" in thistuple:
+    print("yes,,apple is present")
