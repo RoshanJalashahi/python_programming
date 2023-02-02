53c41d125540f085291c9f23b6fc20f20bf49a1e
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 22:09:36 +0545

    tuples_looping

---
+++ python_loopingtuples.py
@@ -0,0 +1,13 @@
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+for x in fruits:
+    print(x)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+for i in range(len(fruits)):
+    print(fruits[i])
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+i = 0
+while i< len(fruits):
+    print(fruits[i])
+    i = i+1
