56253d84be9305c8a7c5e447cd1e20689078fb6d
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-02 22:12:15 +0545

    tuples_joining

---
+++ python_joiningtuples.py
@@ -0,0 +1,8 @@
+tuple1 = ("apple", "mango", "papaya", "pineapple", "cherry")
+tuple2 = ("prabina","loves","Roshan")
+tuple3 =tuple1+tuple2
+print(tuple3)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+mytuple = fruits*3
+print(mytuple)
