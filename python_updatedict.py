c11f6df4d4212c611f793611e638f2047a27887f
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-03 22:43:29 +0545

    set_dictionaryupdate

---
+++ python_updatedict.py
@@ -0,0 +1,10 @@
+thisdicton = {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "hate",
+    "Year" : "2023 - 10 -20"
+}
+thisdicton["status"] = "love"
+print(thisdicton)
+thisdicton.update({"status" : "love"})
+print(thisdicton)
