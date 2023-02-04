43ad497dea760297619646fc37643c0b06018ab4
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-04 22:17:09 +0545

    set_nesteddict

---
+++ python_nesteddict.py
@@ -0,0 +1,47 @@
+thisdicton = {
+    "parbati1" : {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "love",
+    "Year" : "2023 - 10 -21"
+    },
+    "prabina" : {
+        "name" : "prabina",
+        "home" : "dhading",
+        "age" : 17
+    },
+    "prapti" : {
+        "name" : "prapti",
+        "home" : "dhading",
+        "age" : 6
+    }
+}
+print(thisdicton["parbati1"])
+print(thisdicton)
+
+parbati = {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "love",
+    "Year" : "2023 - 10 -21"
+    }
+prabina = {
+        "name" : "prabina",
+        "home" : "dhading",
+        "age" : 17
+    }
+
+prapti = {
+    "name": "prapti",
+    "home": "dhading",
+    "age": 6
+}
+
+myfamily = {
+    "parbati" : parbati,
+    "praboina" : prabina,
+    "prapti" : prapti
+}
+
+print(myfamily["parbati"])
+
