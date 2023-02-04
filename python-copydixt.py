b5c3e6035244d945f942b4ad0ba612fd17a8bdb8
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-04 22:07:00 +0545

    set_copydict

---
+++ python-copydixt.py
@@ -0,0 +1,10 @@
+thisdicton = {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "love",
+    "Year" : "2023 - 10 -21"
+}
+parbati = thisdicton.copy()
+print(parbati)
+parbati = dict(thisdicton)
+print(parbati)
