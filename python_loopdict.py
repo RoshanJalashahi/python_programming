c3dcdb02357fa724de089718a762d1079aa42a3f
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-04 22:02:00 +0545

    set_dictionaryloop

---
+++ python_loopdict.py
@@ -0,0 +1,24 @@
+thisdicton = {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "love",
+    "Year" : "2023 - 10 -21"
+}
+for x in thisdicton:
+    print(x)
+
+for x in thisdicton:
+    print(thisdicton[x])
+
+thisdicton = {
+    "name" : "Parbati",
+    "home" : "Dhading",
+    "status" : "love",
+    "Year" : "2023 - 10 -21"
+}
+for x in thisdicton.values():
+    print(x)
+for x in thisdicton.keys():
+    print(x)
+for x,y in thisdicton.items():
+    print(x,y)
