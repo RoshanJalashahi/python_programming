0aeb530714032c83f38e1a3b15fff8e167c60219
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-05 21:48:44 +0545

    python_whilelooooop

---
+++ python_whileloop.py
@@ -0,0 +1,27 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i ==3:
+        break
+    i = i+1
+
+
+i = 1
+while i < 6:
+    print(i)
+    i = i+1
+    if i ==3:
+        continue
+    print(i)
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer greater than 6")
