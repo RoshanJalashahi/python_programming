3a021e34421cdf6dd828a53ac6e874ffe69407fc
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-07 22:21:48 +0545

    python_functions

---
+++ python_function.py
@@ -0,0 +1,27 @@
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_fun(fname):
+    print(fname+" Roshan")
+
+my_fun("prabina")
+my_fun("parbati")
+
+def myy(fname,lname):
+    print(fname+" "+lname)
+
+myy("prabina","roshan")
+myy("parbati","roshan")
+
+
+def mykids(*kids):
+    print("the youngest kids is "+kids[2])
+mykids("shrisha","shirish","ajanbahu")
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
