378b5abab453875396ea0f6dac382df70c5969ae
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-08 21:31:49 +0545

    python_DATETIME

---
+++ python_datetime.py
@@ -0,0 +1,18 @@
+import datetime
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A"))
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
+import datetime
+
+x = datetime.datetime(2023, 2, 8)
+
+print(x.strftime("%B"))
