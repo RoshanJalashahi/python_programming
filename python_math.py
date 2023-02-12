5acca680f7224c4a50377b782152cc2cfbaa0ef5
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-12 21:40:29 +0545

    python_maths

---
+++ python_math.py
@@ -0,0 +1,21 @@
+x = min(20,5,30)
+y = max(40,60,100)
+
+print(x)
+print(y)
+
+z = abs(-7.25)
+print(z)
+
+x = pow(4,3)
+print(x)
+
+import math
+x = math.sqrt(64)
+print(x)
+x = math.pi
+print(x)
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x)
+print(y)
