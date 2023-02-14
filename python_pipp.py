c92be3fbc05b01b62935721ad68293d52d7abb92
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-14 22:36:16 +0545

    python_piptechniques

---
+++ python_pipp.py
@@ -0,0 +1,14 @@
+'''Check if PIP is Installed
+Navigate your command line to the location of Python's script directory, and type the following:
+
+ExampleGet your own Python Server
+Check PIP version:
+
+C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip --version'''
+'''import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))'''
