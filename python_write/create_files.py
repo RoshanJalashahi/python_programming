22e022c9b68c998eea9e6a1c633754a45a1450ea
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-16 16:23:39 +0545

    python_filecreations

---
+++ python_write/create_files.py
@@ -0,0 +1,19 @@
+'''
+To write to an existing file, you must add a parameter to the open() function:
+
+"a" - Append - will append to the end of the file
+
+"w" - Write - will overwrite any existing content
+
+
+'''
+
+#f = open("Prabina2.txt", "a")
+#f.write("tmi sadhai khusi rahirahanu, yehi ho mero subhakamana")
+#f.close()
+
+#f = open("Prabina2.txt", "r")
+#print(f.read())
+#f.close()
+
+f = open("mylov.txt", "w")
