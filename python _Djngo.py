06d66ff5c6ed4a7d6e57eb8cc2db862a1d1e2514
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-02-18 23:31:45 +0545

    python_Django_installation in venv

---
+++ python _Djngo.py
@@ -0,0 +1,11 @@
+'''
+django-admin startproject my_tennis_club to start project
+'''
+
+'''
+Now that you have a Django project, you can run it, and see what it looks like in a browser.
+
+Navigate to the /my_tennis_club folder and execute this command in the command prompt:
+
+py manage.py runserver
+Open a new browser window and type 127.0.0.1:8000 in the address bar.'''
