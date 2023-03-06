938be678a389ba81884e330a828c5dea152b8220
RoshanJalashahi <106482806+RoshanJalashahi@users.noreply.github.com>
2023-03-06 22:48:17 +0545

    python_Django_update_data

---
+++ python_Django/django_add_main.py
@@ -0,0 +1,48 @@
+'''
+MainGet your own Django Server
+my_tennis_club/members/templates/main.html:
+
+{% extends "master.html" %}
+
+{% block title %}
+  My Tennis Club
+{% endblock %}
+
+
+{% block content %}
+  <h1>My Tennis Club</h1>
+
+  <h3>Members</h3>
+
+  <p>Check out all our <a href="members/">members</a></p>
+
+{% endblock %}
+Create new View
+Then create a new view called main, that will deal with incoming requests to root of the project:
+
+my_tennis_club/members/views.py:
+
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+'''
